688632df5acd42335e67bc90a019fc18c807ef8d
ahmedlabib1729 <a.labib1707@gmail.com>
2024-12-23 18:51:26 +0400

    Initial commit

--- add_vat_label_to_partner/models/res_partner.py
+++ add_vat_label_to_partner/models/res_partner.py
@@ -5,3 +5,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     vat_label = fields.Char(string="VAT Label")
+    company_registry_label = fields.Char(
+        string='Company Registry Label',
+        help='This field is used to display a label for the company registry.'
+    )
