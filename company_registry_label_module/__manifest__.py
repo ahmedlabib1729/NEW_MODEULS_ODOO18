eb0bcfa409fa12c1045a991fdcdaf3c6421b1308
ahmedlabib1729 <a.labib1707@gmail.com>
2024-12-23 18:57:11 +0400

    Initial commit

---
+++ company_registry_label_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Company Registry Label Module',
+    'version': '1.0',
+    'summary': 'Adds a Company Registry Label field to res.partner',
+    'category': 'Customization',
+    'author': 'Ahmed Labib',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
--- company_registry_label_module/models/res_partner.py
+++ company_registry_label_module/models/res_partner.py
@@ -1,7 +1,6 @@
-
 from odoo import models, fields
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    vat_label = fields.Char(string="VAT Label")
+    company_registry_label = fields.Char(string="Company Registry Label")
