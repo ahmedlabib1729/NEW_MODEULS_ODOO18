2a662b58f6b65b876d369318db7f9af0d1dc8dd4
ahmedlabib1729 <a.labib1707@gmail.com>
2025-01-04 13:05:28 +0000

    Initial commit

---
+++ Invoice_form_golden_pure/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+'name' : "Golden Pure Invoice Form" ,
+'author' : "Ahmed Labib",
+
+'depends' : ['base', 'stock' ],
+'data' : [
+
+        'report/ir_actions_report.xml',
+        'report/tax_invoice.xml',
+        'report/layout.xml'
+
+        ],
+
+
+
+}
